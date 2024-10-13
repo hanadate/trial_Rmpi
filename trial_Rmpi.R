ee25c46bc6d4eea4760645c3a843605e43a18a42
hanadate <s2421310@u.tsukuba.ac.jp>
2024-10-13 15:37:35 +0900

    Initial commit

---
+++ trial_Rmpi.R
@@ -0,0 +1,20 @@
+# WARNING: Rtools is required to build R packages but is not currently installed. 
+# Please download and install the appropriate version of Rtools before proceeding:
+#   https://cran.rstudio.com/bin/windows/Rtools/
+#   Installing package into ‘C:/Users/shu87/AppData/Local/R/win-library/4.4’
+# (as ‘lib’ is unspecified)
+# trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.4/Rmpi_0.7-2.1.zip'
+
+# Install Rmpi under MPICH2
+# https://www.stats.uwo.ca/faculty/yu/Rmpi/
+library(Rmpi)
+library(doMPI)
+
+cl <- startMPIcluster(count=2)
+registerDoMPI(cl)
+x <- foreach(i=1:2) %dopar% {
+  sqrt(i)
+}
+closeCluster(cl)
+x
+write.csv(x, "x.csv")
